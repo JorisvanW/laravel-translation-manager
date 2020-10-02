8e5d22b3c6bee29d8eed02125f7ba074c0bb736e
Joris van Willigen <me@jorisvanw.me>
2020-10-02 15:25:51 +0200

    Add publish_enabled to config

--- resources/views/index.php
+++ resources/views/index.php
@@ -267,11 +267,13 @@
 <?php endif; ?>
     <?php if (isset($group)) : ?>
         <div class="trans-actions">
+            <?php if (is_callable($configVal = array_get($config, 'publish_enabled', true)) ? $configVal() : $configVal): ?>
             <form class="form-inline form-publish" method="POST" action="<?= action('\Barryvdh\TranslationManager\Controller@postPublish', $group) ?>" data-remote="true" role="form" data-confirm="Are you sure you want to publish the translations group '<?= $group ?>? This will overwrite existing language files.">
                 <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
                 <button type="submit" class="btn btn-info" data-disable-with="Publishing..">Publish translations</button>
                 <a href="<?= action('\Barryvdh\TranslationManager\Controller@getIndex') ?>" class="btn btn-default">Back</a>
             </form>
+            <?php endif; ?>
 
             <a class="btn btn-default" href="#new-translation">+ New translation</a>
         </div>
